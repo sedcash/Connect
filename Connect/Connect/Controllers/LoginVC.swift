dec8c2711c7d87691e1fa6ff3da59ef1bd8c3b21
sed cash <sedrickcashawjr@sedricks-air.attlocal.net>
2018-02-11 18:28:53 -0500

    Created LoginVC

---
+++ Connect/Connect/Controllers/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Connect
+//
+//  Created by Sedrick Cashaw Jr on 2/11/18.
+//  Copyright © 2018 Sedrick Cashaw Jr. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Connect/Connect/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Connect
+//
+//  Created by Sedrick Cashaw Jr on 2/11/18.
+//  Copyright © 2018 Sedrick Cashaw Jr. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
+
