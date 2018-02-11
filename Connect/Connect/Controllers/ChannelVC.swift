68b7188836ee289d13a6ec993268855b943d2155
sed cash <sedrickcashawjr@sedricks-air.attlocal.net>
2018-02-11 14:37:07 -0500

    Added Channel and Chat VC with reveal features

---
+++ Connect/Connect/Controllers/ChannelVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelVC.swift
+//  Connect
+//
+//  Created by Sedrick Cashaw Jr on 2/10/18.
+//  Copyright © 2018 Sedrick Cashaw Jr. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+}
---
+++ Connect/Connect/Controllers/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  Connect
+//
+//  Created by Sedrick Cashaw Jr on 2/10/18.
+//  Copyright © 2018 Sedrick Cashaw Jr. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+}
